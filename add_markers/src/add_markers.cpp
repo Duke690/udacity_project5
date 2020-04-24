f887b0e75a1a8d63f146bdad0b21bda70fb43688
stefan <stefan.dukart@web.de>
2020-04-24 20:36:52 +0000

    Modify add markers to show object at pickup and then at dropoff zone

--- add_markers/src/add_markers.cpp
+++ add_markers/src/add_markers.cpp
@@ -1,6 +1,48 @@
 #include <ros/ros.h>
 #include <visualization_msgs/Marker.h>
 
+visualization_msgs::Marker CreateMarker(double x, double y, int32_t action, std::size_t id = 0)
+{
+    visualization_msgs::Marker marker;
+    // Set the frame ID and timestamp.  See the TF tutorials for information on these.
+    marker.header.frame_id = "map";
+    marker.header.stamp = ros::Time::now();
+
+    // Set the namespace and id for this marker.  This serves to create a unique ID
+    // Any marker sent with the same namespace and id will overwrite the old one
+    marker.ns = "basic_shapes";
+    marker.id = id;
+
+    // Set the marker type.
+    marker.type = visualization_msgs::Marker::CUBE;
+
+    // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
+    marker.action = action;
+
+    // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
+    marker.pose.position.x = x;
+    marker.pose.position.y = y;
+    marker.pose.position.z = 0;
+    marker.pose.orientation.x = 0.0;
+    marker.pose.orientation.y = 0.0;
+    marker.pose.orientation.z = 0.0;
+    marker.pose.orientation.w = 1.0;
+
+    // Set the scale of the marker -- 1x1x1 here means 1m on a side
+    marker.scale.x = 1.0;
+    marker.scale.y = 1.0;
+    marker.scale.z = 1.0;
+
+    // Set the color -- be sure to set alpha to something non-zero!
+    marker.color.r = 0.0f;
+    marker.color.g = 1.0f;
+    marker.color.b = 0.0f;
+    marker.color.a = 1.0;
+
+    marker.lifetime = ros::Duration();
+    return marker;
+}
+
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "basic_shapes");
@@ -15,89 +57,24 @@ int main(int argc, char **argv)
 
     while (ros::ok())
     {
-        if (ros::param::get("pickup_x", pickup_x) && ros::param::get("pickup_y", pickup_y) && ros::param::get("dropoff_x", dropoff_x) && ros::param::get("dropoff_y", dropoff_y))
+        if (ros::param::get("pickup_x", pickup_x) && ros::param::get("pickup_y", pickup_y) &&
+            ros::param::get("dropoff_x", dropoff_x) && ros::param::get("dropoff_y", dropoff_y))
         {
-            std::cout << "Got pickup and dropoff locations - start showing markers now" << std::endl;
+            std::cout << "Got pickup and dropoff locations:\n pickup: " << pickup_x << ", " << pickup_y << "\n dropoff: "
+                      << dropoff_x << ", " << dropoff_y << "\nStart showing markers now" << std::endl;
             break;
         }
         else
         {
-            ros::Duration(.5).sleep();
+            r.sleep();
         }
     }
 
-    // Set our initial shape type to be a cube
-    uint32_t shape = visualization_msgs::Marker::CUBE;
+    marker_pub.publish(CreateMarker(pickup_x, pickup_y, visualization_msgs::Marker::ADD));
+    ros::Duration(5).sleep();
+    marker_pub.publish(CreateMarker(pickup_x, pickup_y, visualization_msgs::Marker::DELETE));
+    ros::Duration(5).sleep();
+    marker_pub.publish(CreateMarker(dropoff_x, dropoff_y, visualization_msgs::Marker::ADD));
 
-    while (ros::ok())
-    {
-        visualization_msgs::Marker marker;
-        // Set the frame ID and timestamp.  See the TF tutorials for information on these.
-        marker.header.frame_id = "map";
-        marker.header.stamp = ros::Time::now();
-
-        // Set the namespace and id for this marker.  This serves to create a unique ID
-        // Any marker sent with the same namespace and id will overwrite the old one
-        marker.ns = "basic_shapes";
-        marker.id = 0;
-
-        // Set the marker type.  Initially this is CUBE, and cycles between that and SPHERE, ARROW, and CYLINDER
-        marker.type = shape;
-
-        // Set the marker action.  Options are ADD, DELETE, and new in ROS Indigo: 3 (DELETEALL)
-        marker.action = visualization_msgs::Marker::ADD;
-
-        // Set the pose of the marker.  This is a full 6DOF pose relative to the frame/time specified in the header
-        marker.pose.position.x = 0;
-        marker.pose.position.y = 0;
-        marker.pose.position.z = 0;
-        marker.pose.orientation.x = 0.0;
-        marker.pose.orientation.y = 0.0;
-        marker.pose.orientation.z = 0.0;
-        marker.pose.orientation.w = 1.0;
-
-        // Set the scale of the marker -- 1x1x1 here means 1m on a side
-        marker.scale.x = 1.0;
-        marker.scale.y = 1.0;
-        marker.scale.z = 1.0;
-
-        // Set the color -- be sure to set alpha to something non-zero!
-        marker.color.r = 0.0f;
-        marker.color.g = 1.0f;
-        marker.color.b = 0.0f;
-        marker.color.a = 1.0;
-
-        marker.lifetime = ros::Duration();
-
-        // Publish the marker
-        while (marker_pub.getNumSubscribers() < 1)
-        {
-            if (!ros::ok())
-            {
-                return 0;
-            }
-            ROS_WARN_ONCE("Please create a subscriber to the marker");
-            sleep(1);
-        }
-        marker_pub.publish(marker);
-
-        // Cycle between different shapes
-        switch (shape)
-        {
-        case visualization_msgs::Marker::CUBE:
-            shape = visualization_msgs::Marker::SPHERE;
-            break;
-        case visualization_msgs::Marker::SPHERE:
-            shape = visualization_msgs::Marker::ARROW;
-            break;
-        case visualization_msgs::Marker::ARROW:
-            shape = visualization_msgs::Marker::CYLINDER;
-            break;
-        case visualization_msgs::Marker::CYLINDER:
-            shape = visualization_msgs::Marker::CUBE;
-            break;
-        }
-
-        r.sleep();
-    }
+    ros::spin();
 }
--- scripts/pick_objects.sh
+++ scripts/pick_objects.sh
@@ -3,4 +3,4 @@ xterm -e "roslaunch turtlebot_gazebo turtlebot_world.launch" &
 sleep 5
 xterm -e "roslaunch turtlebot_gazebo amcl_demo.launch" &
 sleep 5
-xterm -e "roslaunch pick_objects pick_objects.launch"
+xterm -e "roslaunch pick_objects pick_objects.launch" &
